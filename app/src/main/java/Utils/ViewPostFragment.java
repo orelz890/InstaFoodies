7bc9b231b62e2755baaf273392fca3249784d3e1
HarelGiladi <93948749+HarelGiladi@users.noreply.github.com>
2023-06-07 16:07:38 +0300

    for zamler

--- app/src/main/java/Utils/ViewPostFragment.java
+++ app/src/main/java/Utils/ViewPostFragment.java
@@ -1,9 +1,24 @@
 package Utils;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.instafoodies.R;
+
+
 public class ViewPostFragment  extends Fragment {
-    
 
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View View = inflater.inflate(R.layout.fragment_view_post,container,false);
+        return View;
+    }
 }
