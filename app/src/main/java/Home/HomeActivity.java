473ab38fcd4b7a31c07bdd96e8cdfe1116c14d05
Orel Zamler <orelzx13@gmail.com>
2023-06-08 01:04:59 +0300

    Created the Home page :)

--- app/src/main/java/Home/HomeActivity.java
+++ app/src/main/java/Home/HomeActivity.java
@@ -2,6 +2,8 @@ package Home;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -49,8 +51,10 @@ import java.util.List;
 import java.util.Map;
 
 import Chat.ChatsFragment;
+import Chat.ContactsFragment;
 import Chat.MainChatActivity;
 import Chat.MainChatActivity2;
+import Chat.MessageAdapter;
 import Login.LoginActivity;
 import MLKIT.audio.AudioClassificationActivity;
 import MLKIT.helpers.AudioHelperActivity;
@@ -60,9 +64,14 @@ import MLKIT.image.ImageClassificationActivity;
 import MLKIT.object.ObjectDetectionActivity;
 import MLKIT.text.SpamTextDetectionActivity;
 import MLKIT.image.ImageClassificationActivity;
+import Server.RequestUserFeed;
 import Utils.BottomNavigationViewHelper;
 import Utils.SectionsPagerAdapter;
+import Utils.ServerMethods;
 import Utils.UniversalImageLoader;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -71,11 +80,19 @@ public class HomeActivity extends AppCompatActivity {
     private Context mContext = HomeActivity.this;
     private static final int ACTIVITY_NUM = 0;
 
+    // Server
+    private static ServerMethods serverMethods;
+
     //firebase
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private String uid;
 
     private ImageView messenger;
+
+    // Post Displaying
+    private PostAdapter postAdapter;
+    private RecyclerView postList;
 
 
 
@@ -85,82 +102,61 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: starting.");
 
+        serverMethods = new ServerMethods(this);
+        postList = (RecyclerView) findViewById(R.id.rvPostList);
+        postList.setLayoutManager(new LinearLayoutManager(this));
+
 
         setupFirebaseAuth();
         setupImageViews();
-
-
-        InitImageLoader();
         setupBottomNavigationView();
-//        setupViewPager();
 
-//        MoveToThisPage();
+        setupMainFeed();
+
+    }
+
+    private void setupMainFeed() {
+        System.out.println("im in onStart setupMainFeed()");
+
+        serverMethods.retrofitInterface.getProfileFeedPosts(uid).enqueue(new Callback<RequestUserFeed>() {
+            @Override
+            public void onResponse(@NonNull Call<RequestUserFeed> call, @NonNull Response<RequestUserFeed> response) {
+                if (response.isSuccessful()){
+                    System.out.println(TAG + " - setupMainFeed - response.isSuccessful()");
+                    RequestUserFeed requestUserFeed = response.body();
+                    if (requestUserFeed != null){
+                        System.out.println(TAG + " - setupMainFeed - requestUserFeed != null");
+                        postAdapter = new PostAdapter(requestUserFeed);
+                        postList.setAdapter(postAdapter);
+                    }
+                    else {
+                        System.out.println(TAG + " - setupMainFeed - requestUserFeed == null");
+                    }
+                }
+                else {
+                    System.out.println(TAG + " - setupMainFeed - !!response.isSuccessful()");
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<RequestUserFeed> call, @NonNull Throwable t) {
+                System.out.println(TAG + " - setupMainFeed - onFailure: " + t.getMessage());
+            }
+        });
+
     }
 
     private void setupImageViews() {
-
         messenger = (ImageView) findViewById(R.id.iv_messenger);
         messenger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MoveToMessenger();
-            }
+                Intent intent = new Intent(mContext, MainChatActivity2.class);
+                startActivity(intent);            }
         });
     }
 
 
-    private void MoveToThisPage(){
-        Intent intent = new Intent(mContext, forwardActivity.class);
-        startActivity(intent);
-    }
-
-    private void MoveToMessenger(){
-        Intent intent = new Intent(mContext, MainChatActivity2.class);
-        startActivity(intent);
-    }
-
-    private void InitImageLoader(){
-        UniversalImageLoader universalImageLoader = new UniversalImageLoader(mContext);
-        ImageLoader.getInstance().init(universalImageLoader.getConfig());
-    }
-
-    /**
-     * Responsible for adding the 3 tabs: Camera, Home, Messages
-     */
-
-    private void setupViewPager(){
-        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new CameraFragment()); // index 0
-        adapter.addFragment(new HomeFragment()); // index 1
-        adapter.addFragment(new ChatsFragment()); // index 2
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager_container);
-        viewPager.setAdapter(adapter);
-
-//        // Container for all fragments
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
-//        new TabLayoutMediator(tabLayout, viewPager,
-//                (tab, position) -> {
-//                    switch (position) {
-//                        case 0:
-//                            tab.setIcon(R.drawable.ic_camera);
-//                            break;
-//                        case 1:
-//                            tab.setIcon(R.drawable.ic_instafoodies_icon);
-//                            break;
-//                        case 2:
-//                            tab.setIcon(R.drawable.ic_send);
-//                            break;
-//                    }
-//                }
-//        ).attach();
-
-        // setting the icons
-        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
-        tabLayout.getTabAt(1).setIcon(R.drawable.ic_instafoodies_icon);
-        tabLayout.getTabAt(2).setIcon(R.drawable.ic_send);
-
-    }
 
     /**
      * BottomNavigationView setup
@@ -187,6 +183,9 @@ public class HomeActivity extends AppCompatActivity {
         if(user == null){
             Intent intent = new Intent(mContext, LoginActivity.class);
             startActivity(intent);
+        }
+        else{
+            uid = user.getUid();
         }
     }
 
@@ -239,6 +238,7 @@ public class HomeActivity extends AppCompatActivity {
             if (user != null) {
                 updateToken();
                 // User is signed in
+                this.uid = mAuth.getCurrentUser().getUid();
                 Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
             } else {
                 // User is signed out
@@ -251,8 +251,13 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
+
+        System.out.println("im in onStart HomeActivity");
+
         mAuth.addAuthStateListener(mAuthListener);
         checkCurrentUser(mAuth.getCurrentUser());
+
+        setupMainFeed();
     }
     @Override
     public void onStop() {
---
+++ app/src/main/java/Home/PostAdapter.java
@@ -0,0 +1,146 @@
+package Home;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.instafoodies.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import Chat.MessageAdapter;
+import Server.RequestUserFeed;
+import Utils.StringImageAdapter;
+import de.hdodenhof.circleimageview.CircleImageView;
+import models.Post;
+import models.User;
+import models.UserAccountSettings;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder>{
+
+
+    private RequestUserFeed requestUserFeed;
+
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference usersRef;
+    private DocumentReference usersDoc;
+
+
+    public PostAdapter(RequestUserFeed requestUserFeed) {
+        this.requestUserFeed = requestUserFeed;
+    }
+
+
+
+
+
+    @NonNull
+    @Override
+    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.layout_main_posts, viewGroup, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        return new PostViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+        if (requestUserFeed != null) {
+            User user = requestUserFeed.getUser();
+            UserAccountSettings userAccountSettings = requestUserFeed.getAccount();
+            Post post = requestUserFeed.getPost(position);
+
+            // Set the user info & photo
+            if (userAccountSettings != null){
+                System.out.println("PostAdapter - onBindViewHolder - userAccountSettings != null\n Post(" + position + "): " + userAccountSettings.toString());
+                // Set photo
+                String profile_photo = userAccountSettings.getProfile_photo();
+                if (!profile_photo.isEmpty() && !profile_photo.equals("none")) {
+                    Picasso.get().load(profile_photo).into(holder.profile_photo);
+                } else {
+                    holder.profile_photo.setImageResource(R.drawable.profile_image);
+                }
+
+                // Set username
+                holder.username.setText(user.getUsername());
+            }
+            else{
+                System.out.println("PostAdapter - onBindViewHolder - userAccountSettings == null");
+            }
+
+            // Set the post content
+            if (post != null) {
+                System.out.println("PostAdapter - onBindViewHolder - post != null\n Post(" + position + "): " + post.toString());
+
+                // Set the post pictures
+                List<String> image_paths = post.getImage_paths();
+                if (!image_paths.isEmpty()) {
+                    holder.adapter = new StringImageAdapter(image_paths);
+                    holder.post_images.setAdapter(holder.adapter);
+                }
+
+                // Set the post caption
+                holder.post_caption.setText(post.getCaption());
+
+                // Set time
+                holder.post_time_posted.setText(post.getDate_created());
+
+            }
+            else{
+                System.out.println("PostAdapter - onBindViewHolder - post == null");
+            }
+        }
+        else{
+            System.out.println("PostAdapter - onBindViewHolder - requestUserFeed == null");
+        }
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        System.out.println("getItemCount = " + requestUserFeed.size());
+        return requestUserFeed.size();
+    }
+
+    public static class PostViewHolder extends RecyclerView.ViewHolder {
+        public TextView username, image_likes, post_caption, image_comments_link, post_time_posted;
+        public CircleImageView profile_photo;
+        public ImageView ivEllipses, image_heart_red, image_heart, speech_bubble;
+        public ViewPager2 post_images;
+        public StringImageAdapter adapter;
+        public View view;
+
+
+        public PostViewHolder(@NonNull View itemView) {
+            super(itemView);
+            view = itemView;
+            username = (TextView) itemView.findViewById(R.id.username); // <<<
+            profile_photo = (CircleImageView) itemView.findViewById(R.id.profile_photo); // <<<
+            ivEllipses = (ImageView) itemView.findViewById(R.id.ivEllipses);
+            post_images = (ViewPager2) itemView.findViewById(R.id.post_images); // <<<< ///
+            image_heart_red = (ImageView) itemView.findViewById(R.id.image_heart_red);
+            image_heart = (ImageView) itemView.findViewById(R.id.image_heart);
+            speech_bubble = (ImageView) itemView.findViewById(R.id.speech_bubble);
+            image_likes = (TextView) itemView.findViewById(R.id.image_likes);
+            post_caption = (TextView) itemView.findViewById(R.id.post_caption); // <<<<
+            image_comments_link = (TextView) itemView.findViewById(R.id.image_comments_link);
+            post_time_posted = (TextView) itemView.findViewById(R.id.post_time_posted); // <<<<
+        }
+    }
+
+}
